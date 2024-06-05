c4185c2b7e57ca48458ca3949256e766e19f591a
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2024-06-05 15:51:27 +0300

    new input system implamentation

---
+++ Assets/New Input System/PlayerInputHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public PlayerInput playerInput;
+
+    
+    // WASD ile hareket new input system kullanarak hareket
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        playerInput.Player.Enable();
+    }
+    public Vector3 GetMovementInput()
+    {
+        Vector2 movementInput = playerInput.Player.Move.ReadValue<Vector2>();
+        return new Vector3(movementInput.x, 0.0f, movementInput.y);
+    }
+
+}
--- Assets/TestCodes/PlayerMovementController.cs
+++ Assets/TestCodes/PlayerMovementController.cs
@@ -1,21 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float jumpSpeed = 5.0f;
     [SerializeField] private Rigidbody rigidbody;
-    
+    [SerializeField] private PlayerInputHandler playerInputHandler;
+
+    private void Start()
+    {
+        playerInputHandler.playerInput.Player.Jump.performed +=Jump;
+    }
+
+    private void OnDisable()
+    {
+        playerInputHandler.playerInput.Player.Jump.performed -=Jump;
+    }
 
     // WASD ile hareket
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        var movementInput = playerInputHandler.GetMovementInput();
+        rigidbody.velocity = new Vector3(movementInput.x * speed, rigidbody.velocity.y, movementInput.z * speed);
+    }
 
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
-        rigidbody.velocity = movement * speed;
-        
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        if (obj.ReadValueAsButton())
+        {
+            rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
     }
 }
